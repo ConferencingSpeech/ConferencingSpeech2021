4d1fb183d577154fcf11ad3e91edb063d26390de
jvyvkai <1826402321@qq.com>
2021-02-08 01:28:01 +0800

    update run_track2.sh

--- eval/run_track2.sh
+++ eval/run_track2.sh
@@ -1,17 +1,13 @@
-#!/bin/bash
-
 tmp_path='./tmp/tmp_track2'
 enh_dir='/data/track2/new_{}/'
 ref_dir='/data/simu_task2/dev_task2/{}/ref/'
 mix_dir='/data/simu_task2/dev_task2/{}/mix/'
 result_list='./result/track2_result.csv'
 
-for dir in simu_circular simu_linear_nonuniform simu_linear_uniform ; do
-    python track2_eval.py \
-        --write_path=${tmp_path} \
-        --enh_dir=${enh_dir} \
-        --mix_dir=${mix_dir}
-done
+python track2_eval.py \
+    --write_path=${tmp_path} \
+    --enh_dir=${enh_dir} \
+    --mix_dir=${mix_dir}
 
 python track2_eval_two.py \
     --read_path=${tmp_path} \
